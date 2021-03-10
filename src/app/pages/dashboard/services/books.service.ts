d5a17b2d78d86842fc8903a9286020e30117221d
Alejandro Perdomo <alejandro.perdomob@perficient.com>
2021-03-09 23:46:23 -0500

    feat: add CRUD operations

---
+++ src/app/pages/dashboard/services/books.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { IBook } from '../interfaces/IBook';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+  urlBase = 'http://localhost:8080';
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<IBook[]> {
+    return this.http.get<IBook[]>(`${this.urlBase}/books`);
+  }
+
+  deleteBook(id: string) {
+    return this.http.delete(`${this.urlBase}/books/${id}`);
+  }
+
+  createBook({name, author}: { name: string, author: string}) {
+    return this.http.post<IBook>(`${this.urlBase}/books`, { name, author }, this.httpOptions);
+  }
+
+  updateBook({id, name, author}: { id: string, name: string, author: string}) {
+    return this.http.put<IBook>(`${this.urlBase}/books/${id}`, { name, author }, this.httpOptions);
+  }
+}
